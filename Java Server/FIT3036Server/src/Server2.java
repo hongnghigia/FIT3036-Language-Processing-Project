0a2bca43570c3f5f2d4615261818bd960f50b9e0
mingbing <mingbing@59650aaf-ffea-42e3-8e5c-ebf35a93b945>
2016-08-14 11:15:46 +0000

    server now outputs to client

--- Java Server/FIT3036Server/src/Server2.java
+++ Java Server/FIT3036Server/src/Server2.java
@@ -1,4 +1,5 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,17 +9,28 @@ public class Server2 {
 	ServerSocket server;
 	Socket clientSocket;
 	DataInputStream DIS;
+	DataOutputStream DOS;
 	String receivedFromClient;
+	String fixedString;
 	
 	public void runServer() {
 		System.out.println("Waiting for connection...\n");
 		try {
+			// creates the server and waits for a client to connect
 			server = new ServerSocket(1234);
 			clientSocket = server.accept();
-			System.out.println("Client has connected");
+			
+			// sets up input and output streams between client and server
 			DIS = new DataInputStream(clientSocket.getInputStream());
+			DOS = new DataOutputStream(clientSocket.getOutputStream());
+			
+			// prints what the client sent to the server, and sends to client
 			receivedFromClient = DIS.readUTF();
-			System.out.println(receivedFromClient);
+			fixedString = receivedFromClient.substring(1, receivedFromClient.length()-1);
+			System.out.println(fixedString);
+			DOS.writeUTF(fixedString);
+			
+			// closes connections
 			clientSocket.close();
 			server.close();
 		}
