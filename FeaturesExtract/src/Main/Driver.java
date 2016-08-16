c668a907242934e0b7e16d4c72661af8d30fe073
mingbing <mingbing@59650aaf-ffea-42e3-8e5c-ebf35a93b945>
2016-08-16 11:17:09 +0000

    filtered out characters from all_tagged.txt

--- FeaturesExtract/src/Main/Driver.java
+++ FeaturesExtract/src/Main/Driver.java
@@ -1,6 +1,9 @@
 package Main;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Driver {
@@ -9,15 +12,23 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Checker c1 = new Checker();
+		String[] tags = {":O", ":P", ":L", ":S", ":A", ":N", ": O", ": P", ": L", ": S", ": A", ": N"};
+		
 		try {
-			sc = new Scanner(new File("GIA NGHI.txt"));
+			//sc = new Scanner(new File("GIA NGHI.txt"));
+			String content = new Scanner(new File("All_tagged.txt")).useDelimiter("\\Z").next();
+			content = content.replace("|", "");
+			for (String s : tags) {
+				content = content.replace(s, "");
+			}
+			System.out.println(content);
 		} catch (FileNotFoundException e){
 			e.printStackTrace();
-		}
+		} 
 		
-		while(sc.hasNext()){
-			String word = sc.next();
-			c1.check(word);
-		}
+//		while(sc.hasNext()){
+//			String word = sc.next();
+//			c1.check(word);
+//		}
 	}
 }
