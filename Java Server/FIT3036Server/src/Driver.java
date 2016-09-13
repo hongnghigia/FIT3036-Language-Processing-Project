e5e4a8907bca8b31621442b41ca63a2b09d378ee
mingbing <mingbing@59650aaf-ffea-42e3-8e5c-ebf35a93b945>
2016-09-13 16:04:02 +0000

    added test code for XML file generation

--- Java Server/FIT3036Server/src/Driver.java
+++ Java Server/FIT3036Server/src/Driver.java
@@ -1,13 +1,20 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Driver {
 	
 	public static void main(String[] args) throws IOException {
 		
-//		Server server = new Server();
-//		server.runServer();
-//		
-		Test newTest = new Test();
-		newTest.runTest();
+		Server server = new Server();
+		
+//		// TESTING CODE: 
+//		// uncomment to test XML file generation using sentence from user input
+//		System.out.println("Enter a sentence (e.g. the mug on the table)...");
+//		Scanner sc = new Scanner(System.in);
+//		String s = sc.nextLine();
+//		sc.close();
+//		server.handleReceived(s);
+		
+		server.runServer();
 	}
 }
