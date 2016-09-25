aa6ed5fc899079493561bfa5cd431e9ff7291e4b
pepper-lunch <pepper-lunch@59650aaf-ffea-42e3-8e5c-ebf35a93b945>
2016-09-25 12:14:24 +0000

    - added ICG.java - Functioning KB processor function

---
+++ Java Server/FIT3036Server/src/ICG/Icg.java
@@ -0,0 +1,18 @@
+package ICG;
+import org.drools.*;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+import UCG.*;
+
+public class Icg {
+	
+
+}
---
+++ Java Server/FIT3036Server/src/ICG/KbProcessor.java
@@ -0,0 +1,53 @@
+package ICG;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.*;
+
+
+public class KbProcessor {
+	private ArrayList<String> properties = new ArrayList<String>();
+	private String filename;
+	public KbProcessor(String filename){
+		this.filename = filename;
+	}
+	
+	public ArrayList<String> getProperties(String target){
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(this.filename));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+		try{
+			String line = br.readLine();
+			while(line != null){
+//				System.out.println(line);
+				String[] tmp = line.split(" ");
+				if (tmp[0].equals("define")){
+					String[] parts = line.split(" ");
+					String[] obj = parts[2].split("\\:");
+					if (obj[1].equals(target)){
+						this.properties.add(obj[0]);
+						line = br.readLine();
+					} else {
+						line = br.readLine();
+					}
+				} else {
+					line = br.readLine();
+				}
+			}
+			br.close();
+			return this.properties;
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		return this.properties;
+		
+	}
+}
