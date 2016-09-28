8cc5f1b81718d509c2b54cba4f05c746a61e2d44
mingbing <mingbing@59650aaf-ffea-42e3-8e5c-ebf35a93b945>
2016-09-28 17:17:49 +0000

    added more comments

--- Java Server/FIT3036Server/src/Relations/LocationOn.java
+++ Java Server/FIT3036Server/src/Relations/LocationOn.java
@@ -14,42 +14,60 @@ public class LocationOn extends Relation {
 		
 		// if object on top of landmark
 		if (obj.getMinZ() == lm.getMaxZ()) {
+			
 			// width overlap
+			// if object <= left edge of landmark
 			if (obj.getMinX() < lm.getMinX()) {
+				// if object is completely off landmark
 				if (obj.getMaxX() <= lm.getMinX()) {
 					overlapW = 0.0;
 				}
+				// calculate overlap on left edge
 				else {
 					overlapW = obj.getMaxX() - lm.getMinX();
 				}
 			}
+			
+			// if object > left edge of landmark
 			else {
+				// if object is completely on landmark
 				if (obj.getMaxX() <= lm.getMaxX()) {
 					overlapW = obj.getW();
 				}
+				// if object is completely off landmark
 				else if (obj.getMinX() >= lm.getMaxX()) {
 					overlapW = 0.0;
 				}
+				// calculate overlap on right edge
 				else {
 					overlapW = lm.getMaxX() - obj.getMinX();
 				}
 			}
+			
 			// depth overlap
+			// if object <= back edge of landmark
 			if (obj.getMinY() < lm.getMinY()) {
+				// if object is completely off landmark
 				if (obj.getMaxY() <= lm.getMinY()) {
 					overlapD = 0.0;
 				}
+				// calculate overlap on back edge
 				else {
 					overlapD = obj.getMaxY() - lm.getMinY();
 				}
 			}
+			
+			// if object > back edge of landmark
 			else {
+				// if object is completely on landmark
 				if (obj.getMaxY() <= lm.getMaxY()) {
 					overlapD = obj.getD();
 				}
+				// if object is completely off landmark
 				else if (obj.getMinY() >= lm.getMaxY()) {
 					overlapD = 0.0;
 				}
+				// calculate overlap on front edge
 				else {
 					overlapD = lm.getMaxY() - obj.getMinY();
 				}
