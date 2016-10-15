638823ae87117a0ed27dfee5b89a1b000fff82ef
mingbing <mingbing@59650aaf-ffea-42e3-8e5c-ebf35a93b945>
2016-10-15 15:41:54 +0000

    templates for projective relations

---
+++ Java Server/FIT3036Server/src/Relations/Projective.java
@@ -0,0 +1,8 @@
+package Relations;
+
+import UCG.ICGNode;
+
+public abstract class Projective {
+	
+	abstract double evaluate(ICGNode obj, ICGNode lm, ICGNode speaker);
+}
---
+++ Java Server/FIT3036Server/src/Relations/ProjectiveLeft.java
@@ -0,0 +1,11 @@
+package Relations;
+
+import UCG.ICGNode;
+
+public class ProjectiveLeft extends Projective{
+
+	@Override
+	double evaluate(ICGNode obj, ICGNode lm, ICGNode speaker) {
+		return 0.0;
+	}
+}
