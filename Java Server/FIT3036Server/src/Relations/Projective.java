ac430dd155e8238eb6a26cb74ddac4490b6d90ab
mingbing <mingbing@59650aaf-ffea-42e3-8e5c-ebf35a93b945>
2016-10-16 13:24:46 +0000

    majority of 'back of' relation completed

--- Java Server/FIT3036Server/src/Relations/Projective.java
+++ Java Server/FIT3036Server/src/Relations/Projective.java
@@ -5,4 +5,46 @@ import UCG.ICGNode;
 public abstract class Projective {
 
 	abstract double evaluate(ICGNode obj, ICGNode lm, ICGNode speaker);
+	
+	protected String getQuadrant(double determinant1, double determinant2) {
+		// speaker is bottom left
+		if ((determinant1 == 0) && (determinant2 > 0)) {
+			return "bottomleft";
+		}
+		
+		// speaker is top right
+		else if ((determinant1 == 0) && (determinant2 < 0)) {
+			return "topright";
+		}
+		
+		// speaker is top left
+		else if ((determinant1 < 0) && (determinant2 == 0)) {
+			return "topleft";
+		}
+		
+		// speaker is bottom right
+		else if ((determinant1 > 0) && (determinant2 == 0)) {
+			return "bottomright";
+		}
+		
+		// speaker is bottom
+		else if ((determinant1 > 0) && (determinant2 > 0)) {
+			return "bottom";
+		}
+		
+		// speaker is right
+		else if ((determinant1 > 0) && (determinant2 < 0)) {
+			return "right";
+		}
+		
+		// speaker is left
+		else if ((determinant1 < 0) && (determinant2 > 0)) {
+			return "left";
+		}
+		
+		// speaker is top
+		else {
+			return "top";
+		}
+	}
 }
--- Java Server/FIT3036Server/src/Relations/ProjectiveBack.java
+++ Java Server/FIT3036Server/src/Relations/ProjectiveBack.java
@@ -2,10 +2,177 @@ package Relations;
 
 import UCG.ICGNode;
 
-public class ProjectiveBack extends Projective{
+public class ProjectiveBack extends Projective {
+
+	private double centerXS;
+	private double centerYS;
+	private double centerXO;
+	private double centerYO;
+	private double centerXL;
+	private double centerYL;
+	private double aX;
+	private double aY;
+	private double bX;
+	private double bY;
+	private double cX;
+	private double cY;
+	private double dX;
+	private double dY;
+	private double determinant1;
+	private double determinant2;
+	private double determinant3;
+	private double determinant4;
+	private String locationSpeaker;
+	private String locationObject;
+	private double backX;
+	private double backY;
+	private double multiplier;
 	
 	@Override
 	double evaluate(ICGNode obj, ICGNode lm, ICGNode speaker) {
+		// find center of speaker
+		centerXS = speaker.getMinX() + (speaker.getW() * 0.5);
+		centerYS = speaker.getMinY() + (speaker.getD() * 0.5);
+		
+		// find center of object
+		centerXO = obj.getMinX() + (obj.getW() * 0.5);
+		centerYO = obj.getMinY() + (obj.getD() * 0.5);
+		
+		// find center of landmark
+		centerXL = lm.getMinX() + (lm.getW() * 0.5);
+		centerYL = lm.getMinY() + (lm.getD() * 0.5);
+		
+		// find corners of landmark
+		aX = lm.getMinX();
+		aY = lm.getMaxY();
+		bX = lm.getMaxX();
+		bY = lm.getMinY();
+		cX = lm.getMinX();
+		cY = lm.getMinY();
+		dX = lm.getMaxX();
+		dY = lm.getMaxY();
+		
+		// find determinants of speaker
+		determinant1 = ((bX - aX) * (centerYS - aY)) - ((bY - aY) * (centerXS - aX));
+		determinant2 = ((dX - cX) * (centerYS - cY)) - ((dY - cY) * (centerXS - cX));
+		
+		// find determinants of object
+		determinant3 = ((bX - aX) * (centerYO - aY)) - ((bY - aY) * (centerXO - aX));
+		determinant4 = ((dX - cX) * (centerYO - cY)) - ((dY - cY) * (centerXO - cX));
+		
+		// using determinants to find which quadrant the speaker and object are in
+		locationSpeaker = getQuadrant(determinant1, determinant2);
+		locationObject = getQuadrant(determinant3, determinant4);
+		
+		// find the back edge/corner
+		if (locationSpeaker.equals("top")) {
+			backX = centerXL;
+			backY = lm.getMaxY();
+		}
+		else if (locationSpeaker.equals("bottom")) {
+			backX = centerXL;
+			backY = lm.getMinY();
+		}
+		else if (locationSpeaker.equals("left")) {
+			backX = lm.getMaxX();
+			backY = centerYL;
+		}
+		else if (locationSpeaker.equals("right")) {
+			backX = lm.getMinX();
+			backY = centerYL;
+		}
+		else if (locationSpeaker.equals("topleft")) {
+			backX = lm.getMaxX();
+			backY = lm.getMaxY();
+		}
+		else if (locationSpeaker.equals("topright")) {
+			backX = lm.getMinX();
+			backY = lm.getMaxY();
+		}
+		else if (locationSpeaker.equals("bottomleft")) {
+			backX = lm.getMaxX();
+			backY = lm.getMinY();
+		}
+		else if (locationSpeaker.equals("bottomright")) {
+			backX = lm.getMinX();
+			backY = lm.getMinY();
+		}
+		
+		// get score multiplier
+		if (isBack(locationSpeaker, locationObject, centerXO, centerYO, centerXL, centerYL)) {
+			// object is back of and off the table
+			if (isOff(locationSpeaker, centerXO, centerYO, lm.getMinX(), lm.getMaxX(), lm.getMinY(), lm.getMaxY())) {
+				multiplier = 1.0;
+			}
+			// object is back of and on the table
+			else {
+				multiplier = 0.75;
+			}
+		}
+		// object is not back of
+		else {
+			multiplier = 0.5;
+		}
+		
+		
+		
 		return 0.0;
 	}
+	
+	private boolean isBack(String sp, String obj, double centerXO, double centerYO, double centerXL, double centerYL) {
+		if (sp.equals("bottom") && obj.equals("top")) {
+			return true;
+		}
+		else if (sp.equals("top") && obj.equals("bottom")) {
+			return true;
+		}
+		else if (sp.equals("left") && obj.equals("right")) {
+			return true;
+		}
+		else if (sp.equals("right") && obj.equals("left")) {
+			return true;
+		}
+		else if (sp.equals("bottomleft") && (centerXO > centerXL) && (centerYO < centerYL)) {
+			return true;
+		}
+		else if (sp.equals("topright") && (centerXO < centerXL) && (centerYO > centerYL)) {
+			return true;
+		}
+		else if (sp.equals("topleft") && (centerXO > centerXL) && (centerYO > centerYL)) {
+			return true;
+		}
+		else if (sp.equals("bottomright") && (centerXO < centerXL) && (centerYO < centerYL)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isOff(String sp, double centerXO, double centerYO, double minXL, double maxXL, double minYL, double maxYL) {
+		if (sp.equals("bottom") && (centerYO < minYL)) {
+			return true;
+		}
+		else if (sp.equals("top") && (centerYO > maxYL)) {
+			return true;
+		}
+		else if (sp.equals("left") && (centerXO > maxXL)) {
+			return true;
+		}
+		else if (sp.equals("right") && (centerXO < minXL)) {
+			return true;
+		}
+		else if (sp.equals("bottomleft") && (centerYO < minYL) && (centerXO > maxXL)) {
+			return true;
+		}
+		else if (sp.equals("topright") && (centerYO > maxYL) && (centerXO < minXL)) {
+			return true;
+		}
+		else if (sp.equals("topleft") && (centerYO > maxYL) && (centerXO > maxXL)) {
+			return true;
+		}
+		else if (sp.equals("bottomright") && (centerYO < minYL) && (centerXO < minXL)) {
+			return true;
+		}
+		return false;
+	}
+	
 }
