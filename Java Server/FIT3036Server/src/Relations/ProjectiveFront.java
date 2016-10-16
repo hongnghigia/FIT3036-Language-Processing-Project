6d456d6ea6424eaa82e89ddc0b2323cded62bf71
pepper-lunch <pepper-lunch@59650aaf-ffea-42e3-8e5c-ebf35a93b945>
2016-10-16 13:26:06 +0000

    - updated projectiveFront, still need to do when speaker is on the diag line

--- Java Server/FIT3036Server/src/Relations/ProjectiveFront.java
+++ Java Server/FIT3036Server/src/Relations/ProjectiveFront.java
@@ -1,13 +1,129 @@
 package Relations;
 
 import UCG.ICGNode;
-import ICG.KbProcessor;
+
+import java.awt.Point;
 
 public class ProjectiveFront extends Projective{
+	Point point1, point2, point3, point4;
+	Point pointA, pointB, pointC, pointD;
+	double distance;
+	double eval;
 	@Override
 	double evaluate(ICGNode obj, ICGNode lm, ICGNode speaker) {
+		// -------------
+		// 1 		   3
+		//
+		// 2		   4
+		// -------------
 		
-		return 0;
+		// Initializing the corner points for the landmark
+		point1 = new Point();
+		point1.setLocation(lm.getMinX(), lm.getMinY());
+		
+		point3 = new Point();
+		point3.setLocation(lm.getMaxX(), lm.getMinY());
+		
+		point2 = new Point();
+		point2.setLocation(lm.getMinX(), lm.getMaxY());
+		
+		point4 = new Point();
+		point4.setLocation(lm.getMaxX(), lm.getMaxY());
+		
+		// check where the speaker is
+		// middle point of the speaker
+		double midSX = (speaker.getW()/2) + speaker.getMinX();
+		double midSY = (speaker.getD()/2) + speaker.getMinY();
+		
+		double objmidX = (obj.getW()/2) + obj.getMinX();
+		double objmidY = (obj.getD()/2) + obj.getMinY();
+		// speaker coordinates
+		Point speakerPoint = new Point();
+		speakerPoint.setLocation(midSX, midSY);
+		
+		Point objpoint = new Point();
+		objpoint.setLocation(objmidX, objmidY);
+		
+		double score14 = determinantScore(point1, point4, speakerPoint);
+		double score23 = determinantScore(point2, point3, speakerPoint);
+		
+		// checking the determinant score
+		if (score14 == 1 && score23 == 1){ // the speaker at the bottom quadrant
+			double objscore14 = determinantScore(point1, point4, objpoint);
+			double objscore23 = determinantScore(point2, point3, objpoint);
+			
+			if(objscore14 == 1 && objscore23 == 1){ // if the object is also in the same quadrant as speaker
+				double midEdgeX = lm.getW()/2;
+				double midEdgeY = lm.getMaxY();
+				
+				distance = Math.hypot(objmidX - midEdgeX, objmidY - midEdgeY);
+			}
+		} else if (score14 == 1 && score23 == -1) { // Left quadrant
+			double objscore14 = determinantScore(point1, point4, objpoint);
+			double objscore23 = determinantScore(point2, point3, objpoint);
+			
+			if(objscore14 == 1 && objscore23 == -1){ // if the object is also in the same quadrant as speaker
+				double midEdgeX = lm.getW()/2;
+				double midEdgeY = lm.getMaxY();
+				
+				distance = Math.hypot(objmidX - midEdgeX, objmidY - midEdgeY);
+			}
+		} else if (score14 == -1 && score23 == 1) { // Right quadrant
+			double objscore14 = determinantScore(point1, point4, objpoint);
+			double objscore23 = determinantScore(point2, point3, objpoint);
+			
+			if(objscore14 == -1 && objscore23 == -1){ // if the object is also in the same quadrant as speaker
+				double midEdgeX = lm.getW()/2;
+				double midEdgeY = lm.getMaxY();
+				
+				distance = Math.hypot(objmidX - midEdgeX, objmidY - midEdgeY);
+			}
+		} else if (score14 == -1 && score23 == -1) { // top quadrant
+			double objscore14 = determinantScore(point1, point4, objpoint);
+			double objscore23 = determinantScore(point2, point3, objpoint);
+			
+			if(objscore14 == -1 && objscore23 == -1){ // if the object is also in the same quadrant as speaker
+				double midEdgeX = lm.getW()/2;
+				double midEdgeY = lm.getMaxY();
+				
+				distance = Math.hypot(objmidX - midEdgeX, objmidY - midEdgeY);
+			}
+		}
+		// -----------------
+		//        C
+		//        |
+		// A______|_________B
+		//        |
+		//        |
+		//        D
+		// ------------------
+		pointA = new Point();
+		pointA.setLocation(lm.getMinX(), lm.getD()/2 + lm.getMinY());
+		
+		pointB = new Point();
+		pointB.setLocation(lm.getMaxX(), lm.getD()/2 + lm.getMinY());
+		
+		pointC = new Point();
+		pointC.setLocation(lm.getW()/2 + lm.getMinX(), lm.getMinY());
+		
+		pointD = new Point();
+		pointD.setLocation(lm.getW()/2 + lm.getMinX(), lm.getMaxY());
+		
+		if (score14 == 0 && score23 == 1){
+			
+		} else if (score14 == 0 && score23 == -1){
+			
+		} else if (score14 == 1 && score23 == 0){
+			
+		} else if (score14 == -1 && score23 == 0){
+			
+		}
+		
+		return eval;
+	}
+	
+	private double determinantScore(Point a, Point b, Point c){
+		return ((b.getX() - a.getX()) * (c.getY() - a.getY())) - ((b.getY() - a.getY()) * (c.getX() - a.getX()));
 	}
 
 }
